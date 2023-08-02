b1308c11aa3a98501fd3bf06312888b46f9b024e
dekzyd <adekoyadayo2010@gmail.com>
2023-08-02 14:13:14 +0100

    add tailwind functionality

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>You clicked {count} time(s)</h1>
+      <h1 className="text-3xl font-bold underline text-clifford">
+        You clicked {count} time(s)
+      </h1>
       <button onClick={() => setCount(count + 1)}>Click me!</button>
     </div>
   );
