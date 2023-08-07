5b52a4e956c087bb900da55846f207095e736809
dekzyd <adekoyadayo2010@gmail.com>
2023-08-07 14:38:01 +0100

    all zones working

--- app/javascript/components/Table.jsx
+++ app/javascript/components/Table.jsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom";
 
 const Table = ({ data }) => {
   return (
-    <div class="relative overflow-x-auto">
-      <table class="w-full text-sm text-left text-gray-500">
-        <thead class="text-lg text-gray-700 bg-[#E9F9FD] border-b-2">
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-lg text-gray-700 bg-[#E9F9FD] border-b-2">
           <tr>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Desk ID
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Status
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Campaign
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Action
             </th>
           </tr>
@@ -25,10 +25,10 @@ const Table = ({ data }) => {
           {data.map((desk, index) => {
             const { desk_id, status, campaign, id } = desk;
             return (
-              <tr key={id} class="border-b">
+              <tr key={id} className="border-b">
                 <th
                   scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
                 >
                   <div className="flex flex-col justify-center">
                     {status === "Vacant" ? (
@@ -41,7 +41,7 @@ const Table = ({ data }) => {
                       >
                         <g>
                           <path
-                            class="st0"
+                            className="st0"
                             d="M97.13,61.06c3.25,0,5.88,0.56,5.88,1.25s-2.63,1.25-5.88,1.25c-3.25,0-5.88-0.56-5.88-1.25 S93.88,61.06,97.13,61.06L97.13,61.06z M25.08,14.73v56.31c0,0.58-0.24,1.1-0.61,1.48c-0.38,0.38-0.9,0.61-1.48,0.61h-9.92 c-0.57,0-1.09-0.23-1.47-0.61l0,0l0,0l0,0c-0.38-0.38-0.61-0.9-0.61-1.47V14.73H2.96c-0.82,0-1.56-0.33-2.09-0.87 C0.33,13.32,0,12.58,0,11.77V2.96C0,2.14,0.33,1.4,0.87,0.87S2.14,0,2.96,0h116.96c0.82,0,1.56,0.33,2.09,0.87s0.87,1.28,0.87,2.09 v8.81c0,0.82-0.33,1.56-0.87,2.09c-0.54,0.54-1.28,0.87-2.09,0.87h-0.19v56.31c0,0.57-0.24,1.09-0.61,1.47l-0.01,0.01 c-0.38,0.38-0.9,0.61-1.47,0.61H77.18c-0.58,0-1.1-0.23-1.48-0.61c-0.07-0.07-0.13-0.14-0.19-0.22c-0.27-0.35-0.43-0.79-0.43-1.26 V14.73H25.08L25.08,14.73z M78.54,30.64h37.74V14.73H78.54V30.64L78.54,30.64z M116.28,34.09H78.54V50.3h37.74V34.09L116.28,34.09z M116.28,53.75H78.54v15.93h37.74V53.75L116.28,53.75z M119.43,3.45H3.45v7.83h115.98V3.45L119.43,3.45z M21.63,14.73h-7.21v54.94 h7.21V14.73L21.63,14.73z M97.13,21.27c3.25,0,5.88,0.53,5.88,1.19s-2.63,1.19-5.88,1.19c-3.25,0-5.88-0.53-5.88-1.19 S93.88,21.27,97.13,21.27L97.13,21.27z M97.13,41.13c3.25,0,5.88,0.56,5.88,1.25s-2.63,1.25-5.88,1.25c-3.25,0-5.88-0.56-5.88-1.25 S93.88,41.13,97.13,41.13L97.13,41.13z"
                           />
                         </g>
@@ -70,7 +70,7 @@ const Table = ({ data }) => {
                     <p className="flex-1 whitespace-nowrap">{desk_id}</p>
                   </div>
                 </th>
-                <td class="px-6 py-4">
+                <td className="px-6 py-4">
                   <span
                     className={`${
                       status === "Vacant"
@@ -83,8 +83,8 @@ const Table = ({ data }) => {
                     {status}
                   </span>
                 </td>
-                <td class="px-6 py-4">{campaign}</td>
-                <td class="px-6 py-4 text-sky-700 font-medium">
+                <td className="px-6 py-4">{campaign}</td>
+                <td className="px-6 py-4 text-sky-700 font-medium">
                   <a href={`/zone_ds/${id}/edit`}>EDIT</a>
                 </td>
               </tr>
