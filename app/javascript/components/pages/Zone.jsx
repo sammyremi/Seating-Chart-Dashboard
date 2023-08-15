d8f896c41b5eccd0efd95bce20c755489b8c6279
dekzyd <adekoyadayo2010@gmail.com>
2023-08-15 12:01:03 +0100

    add zonechart layout

--- app/javascript/components/pages/Zone.jsx
+++ app/javascript/components/pages/Zone.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Table from "../Table";
+import D_ZoneChart from "../zone charts/D/D_ZoneChart";
+import E_ZoneChart from "../zone charts/E/E_ZoneChart";
+import H_ZoneChart from "../zone charts/H/H_ZoneChart";
+import I_ZoneChart from "../zone charts/I/I_ZoneChart";
+import J_ZoneChart from "../zone charts/J/J_ZoneChart";
+import K_ZoneChart from "../zone charts/K/K_ZoneChart";
+import L_ZoneChart from "../zone charts/L/L_ZoneChart";
+import M_ZoneChart from "../zone charts/M/M_ZoneChart";
+import N_ZoneChart from "../zone charts/N/N_ZoneChart";
+import Q_ZoneChart from "../zone charts/Q/Q_ZoneChart";
+import R_ZoneChart from "../zone charts/R/R_ZoneChart";
 
 const Zone = () => {
   const { zone_name } = useParams();
@@ -9,6 +20,43 @@ const Zone = () => {
 
   const zone_id_array = zone_name.split("");
   const zone_id = zone_id_array[zone_id_array.length - 2];
+
+  let chartComponent;
+  switch (zone_id.toUpperCase()) {
+    case "D":
+      chartComponent = <D_ZoneChart />;
+      break;
+    case "E":
+      chartComponent = <E_ZoneChart />;
+      break;
+    case "H":
+      chartComponent = <H_ZoneChart />;
+      break;
+    case "I":
+      chartComponent = <I_ZoneChart />;
+      break;
+    case "J":
+      chartComponent = <J_ZoneChart />;
+      break;
+    case "K":
+      chartComponent = <K_ZoneChart />;
+      break;
+    case "L":
+      chartComponent = <L_ZoneChart />;
+      break;
+    case "M":
+      chartComponent = <M_ZoneChart />;
+      break;
+    case "N":
+      chartComponent = <N_ZoneChart />;
+      break;
+    case "Q":
+      chartComponent = <Q_ZoneChart />;
+      break;
+    case "R":
+      chartComponent = <R_ZoneChart />;
+      break;
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,6 +75,7 @@ const Zone = () => {
       <p className="text-3xl font-bold underline">
         Zone {zone_id.toUpperCase()}
       </p>
+      {chartComponent}
       <Table data={data} />
     </div>
   );
--- app/javascript/components/zone charts/D/D_ZoneChart.jsx
+++ app/javascript/components/zone charts/D/D_ZoneChart.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./ZoneDstyle.css";
 
 const D_ZoneChart = () => {
-  return <div>D_ZoneChart</div>;
+  return (
+    <div>
+      <div id="content" class="zoneD">
+        <div class="restroom">restroom</div>
+        <div class="occupied6">occupied6</div>
+        <div class="vacant">vacant</div>
+        <div class="occupied">occupied</div>
+        <div class="occupied2">occupied2</div>
+        <div class="occupied3">occupied3</div>
+        <div class="occupied4">occupied4</div>
+        <div class="occupied5">occupied5</div>
+        <div class="occupied7">occupied7</div>
+        <div class="occupied8">occupied8</div>
+        <div class="occupied9">occupied9</div>
+        <div class="occupied10">occupied10</div>
+        <div class="occupied11">occupied11</div>
+        <div class="damaged">damaged</div>
+        <div class="server">server</div>
+        <div class="hr">hr</div>
+        <div class="door">door</div>
+        <div class="zoneE">zoneE</div>
+      </div>
+    </div>
+  );
 };
 
 export default D_ZoneChart;
