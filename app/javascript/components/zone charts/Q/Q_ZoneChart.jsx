6b3cedda9bbd30c0e0f15d33a397ff50ea4e7962
dekzyd <adekoyadayo2010@gmail.com>
2023-08-18 02:14:11 +0100

    adjust grid chart

--- app/javascript/components/zone charts/Q/Q_ZoneChart.jsx
+++ app/javascript/components/zone charts/Q/Q_ZoneChart.jsx
@@ -3,53 +3,53 @@ import React from "react";
 const Q_ZoneChart = () => {
   return (
     <div>
-      <div class="ZoneQ p-2.5 grid grid-cols-21 grid-rows-10 gap-2.5 text-center">
-        <div class="restroom1 bg-lime-500 col-start-1 col-end-5 row-start-1 row-end-2">
+      <div class="ZoneQ p-2.5 grid grid-cols-22 grid-rows-12 gap-2 text-center">
+        <div class="restroom1 bg-lime-500 col-start-1 col-end-3 row-start-1 row-end-2">
           restroom1
         </div>
         <div class="occupied1 bg-cyan-300 col-start-5 col-end-18 row-start-1 row-end-2">
           occupied1
         </div>
-        <div class="restroom2 bg-lime-500 col-start-18 col-end-22 row-start-1 row-end-2">
+        <div class="restroom2 bg-lime-500 col-start-20 col-end-24 row-start-1 row-end-2">
           restroom2
         </div>
-        <div class="occupied2 bg-cyan-300 col-start-3 col-end-7 row-start-2 row-end-4">
+        <div class="occupied2 bg-cyan-300 col-start-3 col-end-7 row-start-3 row-end-5">
           occupied2
         </div>
-        <div class="occupied3 bg-cyan-300 col-start-8 col-end-13 row-start-2 row-end-4">
+        <div class="occupied3 bg-cyan-300 col-start-9 col-end-14 row-start-3 row-end-5">
           occupied3
         </div>
-        <div class="occupied4 bg-cyan-300 col-start-15 col-end-17 row-start-3 row-end-7">
+        <div class="occupied4 bg-cyan-300 col-start-15 col-end-17 row-start-3 row-end-8">
           occupied4
         </div>
-        <div class="occupied5 bg-cyan-300 col-start-17 col-end-19 row-start-3 row-end-7">
+        <div class="occupied5 bg-cyan-300 col-start-18 col-end-20 row-start-3 row-end-8">
           occupied5
         </div>
-        <div class="occupied6 bg-cyan-300 col-start-19 col-end-20 row-start-4 row-end-6">
+        <div class="occupied6 bg-cyan-300 col-start-21 col-end-22 row-start-4 row-end-6">
           occupied6
         </div>
-        <div class="occupied7 bg-cyan-300 col-start-8 col-end-13 row-start-4 row-end-6">
+        <div class="occupied7 bg-cyan-300 col-start-9 col-end-14 row-start-6 row-end-8">
           occupied7
         </div>
-        <div class="occupied8 bg-cyan-300 col-start-1 col-end-3 row-start-4 row-end-10">
+        <div class="occupied8 bg-cyan-300 col-start-1 col-end-3 row-start-6 row-end-12">
           occupied8
         </div>
-        <div class="occupied9 bg-cyan-300 col-start-3 col-end-6 row-start-5 row-end-10">
+        <div class="occupied9 bg-cyan-300 col-start-4 col-end-6 row-start-7 row-end-12">
           occupied9
         </div>
-        <div class="occupied10 bg-cyan-300 col-start-6 col-end-7 row-start-4 row-end-10">
+        <div class="occupied10 bg-cyan-300 col-start-7 col-end-8 row-start-7 row-end-13">
           occupied10
         </div>
-        <div class="occupied11 bg-cyan-300 col-start-8 col-end-11 row-start-7 row-end-10">
+        <div class="occupied11 bg-cyan-300 col-start-9 col-end-12 row-start-9 row-end-12">
           occupied11
         </div>
-        <div class="occupied12 bg-cyan-300 col-start-12 col-end-13 row-start-7 row-end-10">
+        <div class="occupied12 bg-cyan-300 col-start-13 col-end-14 row-start-9 row-end-12">
           occupied12
         </div>
-        <div class="zoner bg-orange-300 col-start-20 col-end-22 row-start-2 row-end-11">
+        <div class="zoner bg-orange-300 col-start-22 col-end-23 row-start-2 row-end-13">
           Zone R
         </div>
-        <div class="door bg-pink-300 col-start-3 col-end-5 row-start-10 row-end-11">
+        <div class="door bg-pink-300 col-start-3 col-end-5 row-start-12 row-end-13">
           door
         </div>
       </div>
--- app/javascript/components/zone charts/R/R_ZoneChart.jsx
+++ app/javascript/components/zone charts/R/R_ZoneChart.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const R_ZoneChart = () => {
   return (
     <div>
-      <div class="ZoneR p-2.5 grid grid-cols-14 grid-rows-9 gap-2.5 text-center">
+      <div class="ZoneR p-2.5 grid grid-cols-14 grid-rows-12 gap-2.5 text-center">
         <div class="restroom bg-lime-500 col-start-7 col-end-9 row-start-1 row-end-2">
           restroom
         </div>
         <div class="server bg-gray-300 col-start-3 col-end-5 row-start-1 row-end-2">
           server
         </div>
-        <div class="occupied1 bg-cyan-300 col-start-9 col-end-12 row-start-1 row-end-2">
+        <div class="occupied1 bg-cyan-300 col-start-11 col-end-14 row-start-1 row-end-2">
           occupied1
         </div>
         <div class="occupied2 bg-cyan-300 col-start-1 col-end-7 row-start-2 row-end-4">
@@ -19,28 +19,28 @@ const R_ZoneChart = () => {
         <div class="occupied3 bg-cyan-300 col-start-9 col-end-14 row-start-2 row-end-4">
           occupied3
         </div>
-        <div class="occupied4 bg-cyan-300 col-start-9 col-end-14 row-start-4 row-end-6">
+        <div class="occupied4 bg-cyan-300 col-start-9 col-end-14 row-start-5 row-end-7">
           occupied4
         </div>
-        <div class="occupied5 bg-cyan-300 col-start-1 col-end-7 row-start-4 row-end-6">
+        <div class="occupied5 bg-cyan-300 col-start-1 col-end-7 row-start-5 row-end-7">
           occupied5
         </div>
-        <div class="occupied6 bg-cyan-300 col-start-9 col-end-14 row-start-6 row-end-8">
+        <div class="occupied6 bg-cyan-300 col-start-9 col-end-14 row-start-8 row-end-10">
           occupied6
         </div>
-        <div class="occupied7 bg-cyan-300 col-start-1 col-end-7 row-start-7 row-end-8">
+        <div class="occupied7 bg-cyan-300 col-start-1 col-end-5 row-start-10 row-end-11">
           occupied7
         </div>
-        <div class="occupied8 bg-cyan-300 col-start-9 col-end-11 row-start-8 row-end-9">
+        <div class="occupied8 bg-cyan-300 col-start-12 col-end-14 row-start-11 row-end-12">
           occupied8
         </div>
-        <div class="occupied9 bg-cyan-300 col-start-7 col-end-8 row-start-7 row-end-9">
+        <div class="occupied9 bg-cyan-300 col-start-7 col-end-8 row-start-8 row-end-10">
           occupied9
         </div>
-        <div class="zonej bg-orange-300 col-start-14 col-end-15 row-start-1 row-end-10">
+        <div class="zonej bg-orange-300 col-start-14 col-end-15 row-start-1 row-end-13">
           Zone J
         </div>
-        <div class="door bg-pink-300 col-start-8 col-end-9 row-start-9 row-end-10">
+        <div class="door bg-pink-300 col-start-8 col-end-9 row-start-12 row-end-13">
           door
         </div>
       </div>
